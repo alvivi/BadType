56839ab771abf2526e23ca03b52ad1532f6c40cd
Alvaro Vilanova Vidal <alvivi@gmail.com>
2016-01-19 23:01:28 +0100

    Initial Commit

---
+++ BadType/BadType.h
@@ -0,0 +1,19 @@
+//
+//  BadType.h
+//  BadType
+//
+//  Created by Alvaro Vilanova Vidal on 19/1/16.
+//  Copyright © 2016 hacking.bar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BadType.
+FOUNDATION_EXPORT double BadTypeVersionNumber;
+
+//! Project version string for BadType.
+FOUNDATION_EXPORT const unsigned char BadTypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BadType/PublicHeader.h>
+
+
