c6c12b36501434bef2320f32d0549339582cf3de
Alvaro Vilanova Vidal <alvivi@gmail.com>
2016-01-19 23:08:46 +0100

    first commit

---
+++ BadType-TestingApp-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BadType-TestingApp-iOS
+//
+//  Created by Alvaro Vilanova Vidal on 19/1/16.
+//  Copyright © 2016 hacking.bar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
