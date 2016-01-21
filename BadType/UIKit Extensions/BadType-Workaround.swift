7652f72b44439878070bc0106523d70c18af02a7
Alvaro Vilanova Vidal <alvivi@gmail.com>
2016-01-21 13:36:21 +0100

    workaround rdar://23114017

--- BadType/UIKit Extensions/BadType-Workaround.swift
+++ BadType/UIKit Extensions/BadType-Workaround.swift
@@ -2,8 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import UIKit
+// This file is needed to workaround
+// https://openradar.appspot.com/23114017
 
+
+import UIKit
+import BadType
+
+// MARK: - UIButton
 
 extension UIButton: FontStyleable {
   @IBInspectable public var fontStyle: NSString {
@@ -21,5 +27,27 @@ extension UIButton: FontStyleable {
 }
 
 @IBDesignable public class BTButton: UIButton {
+  // Intentionally empty
+}
+
+// MARK: - UILabel
+
+extension UILabel: FontStyleable {
+  @IBInspectable public var fontStyle: NSString {
+    get {
+      return font.fontStyle ?? ""
+    }
+
+    set {
+      if let font = BadType.catalog?.fontWithStyleName(String(newValue), defaultFont: font) {
+        self.font = font
+      }
+      BadType.addStyleable(self)
+    }
+  }
+}
+
+
+@IBDesignable public class BTLabel: UILabel {
   // Intentionally empty
 }
--- BadType/UIKit Extensions/Extensions.swift
+++ BadType/UIKit Extensions/Extensions.swift
@@ -2,8 +2,34 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// This file is needed to workaround
+// https://openradar.appspot.com/23114017
+
+
 import UIKit
 
+// MARK: - UIButton
+
+extension UIButton: FontStyleable {
+  @IBInspectable public var fontStyle: NSString {
+    get {
+      return titleLabel?.fontStyle ?? ""
+    }
+
+    set {
+      if let font = BadType.catalog?.fontWithStyleName(String(newValue), defaultFont: titleLabel?.font) {
+        self.titleLabel?.font = font
+      }
+      BadType.addStyleable(self)
+    }
+  }
+}
+
+@IBDesignable public class BTButton: UIButton {
+  // Intentionally empty
+}
+
+// MARK: - UILabel
 
 extension UILabel: FontStyleable {
   @IBInspectable public var fontStyle: NSString {
@@ -19,6 +45,7 @@ extension UILabel: FontStyleable {
     }
   }
 }
+
 
 @IBDesignable public class BTLabel: UILabel {
   // Intentionally empty
